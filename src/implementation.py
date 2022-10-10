d0b5e8da6c734349d4a483fc1a7acac8e8ef69a3
HLIN <haominglin@outlook.com>
2022-10-11 00:53:21 +0200

    implentation not tested

--- src/implementation.py
+++ src/implementation.py
@@ -1,12 +1,31 @@
 import numpy as np
 
-from Project1.src.helpers import compute_loss
+from helpers import compute_loss
+from helpers import compute_gradient
 
 def least_squares_GD(y, tx, initial_w, max_iters, gamma):
-    pass
+    w = np.copy(initial_w, dtype="float")
+
+    for i in range(max_iters):
+        gradient, _  = compute_gradient(y, tx, w)
+        w -= gamma * gradient
+    loss = compute_loss(y, tx, w)
+
+    return w, loss
+
 
 def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
-    pass
+    w = np.copy(initial_w, dtype="float")
+
+    for i in range(max_iters):
+        # pick a random sample from data set
+        idx = np.random.randint(len(y), size=1)
+        gradient, _ = compute_gradient(y[idx], tx[idx], w)
+        w -= gamma * gradient
+    loss = compute_loss(y, tx, w)
+
+    return w, loss
+
 
 def least_squares(y, tx):
     a = tx.T.dot(tx)
@@ -15,8 +34,14 @@ def least_squares(y, tx):
     loss = compute_loss(y, tx, w)
     return w, loss
 
+
 def ridge_regression(y, tx, lambda_):
-    pass
+    a = tx.T.dot(tx) + 2*len(y)*lambda_*np.eye(tx.shape[1])
+    b = tx.T.dot(y)
+    w = np.linalg.solve(a, b)
+    loss = compute_loss(y, tx, w)
+    return w, loss
+
 
 def logistic_regression(y, tx, initial_w, max_iter, gamma):
     pass
